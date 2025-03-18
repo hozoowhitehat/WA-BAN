3f6df3973aab58a694cb3cab26b47d168237c38b
HOZOO WHITE HAT <hozooimut@gmail.com>
2025-03-18 12:21:29 +0700

    Update ATTACKER.sh

--- ATTACKER.sh
+++ ATTACKER.sh
@@ -1,6 +1,36 @@
 pkg install mpv -y
 clear
-
+e="echo -e "
+s="sleep 1"
+ss="sleep 2"
+sss="sleep 3"
+Version="./pockat/Version"
+versitoolsv5=$(cat "$Version")
+update="./pockat/info"
+gal="/data/data/com.termux/files/usr/lib/$versitoolsv5"
+musik="bash $HOME/WA-BAN/test.sh"
+letakgalirus="/data/data/com.termux/files/usr/etc/apt"
+internalsc="/data/data/com.termux/files/usr/include/jso"
+termux_id=$(whoami)
+versitoolsv5=$(cat "$Version")
+m="\033[1;31m"   # Merah (sudah diberikan)
+h="\033[1;32m"   # Hijau (sudah diberikan)
+k="\033[1;33m"   # Kuning (sudah diberikan)
+b="\033[1;34m"   # Biru (sudah diberikan)
+bl="\033[1;36m"  # Biru Muda (sudah diberikan)
+p="\033[1;37m"   # Putih (sudah diberikan)
+u="\033[1;35m"   # Ungu
+pu="\033[1;30m"  # Abu-abu
+c="\033[1;96m"   # Cyan Terang
+bg_m="\033[41m"  # Latar Belakang Merah
+bg_h="\033[42m"  # Latar Belakang Hijau
+bg_k="\033[43m"  # Latar Belakang Kuning
+bg_b="\033[44m"  # Latar Belakang Biru
+bg_bl="\033[46m" # Latar Belakang Biru Muda
+bg_p="\033[47m"  # Latar Belakang Putih
+bg_u="\033[45m"  # Latar Belakang Ungu
+bg_pu="\033[40m" # Latar Belakang Abu-abu
+res="\033[0m"    # Reset semua atribut
 
 
 RED='\033[0;31m'
